a1f626d0875bec1b2b6de6b80077deaf7b370b09
Ross-dch97 <dchross97@outlook.it>
2024-10-29 23:54:19 +0100

    Selfwork Cicli 3

---
+++ main.js
@@ -0,0 +1,21 @@
+
+
+let bevanda = parseInt (prompt('seleziona bevanda: \n 1 - acqua \n 2 - coca cola \n 3 - birra '));
+
+ if (bevanda == 1) {
+    console.log('Ecco la tua acqua');
+    
+ }else if(bevanda == 2){
+      console.log('Ecco la tua coca cola');
+      
+ }else if (bevanda == 3) {
+    console.log('Ecco la tua birra');
+    
+ }else{
+    console.log(prompt('Scelta non valida : scegli tra 1,2 e 3'));
+    
+ }
+    
+
+ 
+ 
